693940f4e1a32b05a3c47dcedd74988e42379994
sathish <>
2018-12-11 06:40:36 +0530

    specs#ExpenseType # All

--- spec/models/expense_type_spec.rb
+++ spec/models/expense_type_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe ExpenseType, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	before(:all) { 
+		@user = build(:user) 
+		@expense_type = build(:expense_type,creator_id: @user.id)
+	} 		
+
+	context "has proper validations" do
+		it { should validate_presence_of(:title) }
+		it { expect(@expense_type).to validate_uniqueness_of(:title).scoped_to(:creator_id) }
+	end
+
+	context "has proper associations" do
+		it { should belong_to(:creator) }
+	end
+
 end
