4199a8d91d5025795bc5590468016ea51406e19a
sathish <>
2018-12-11 09:02:29 +0530

    adding auth and setting current module for current_user access in models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+	protect_from_forgery with: :exception
+	before_action :set_current_user
+
+	def set_current_user
+  		Current.user = current_user
+	end
+
+
 end
