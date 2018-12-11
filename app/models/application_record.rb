ca25426459d64614c8b0225ed58cc25fbfb30a8c
sathish <>
2018-12-11 09:21:59 +0530

    adding Graphql decorator --> to fetch additional attributes on demand

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,13 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def method_missing(m, *args, &block)
+    decorator = ("GraphqlDecorators::" + self.class.name.demodulize + "Decorator").constantize.new(self) 
+    if decorator.respond_to?(m, *args, &block)
+      decorator.send(m, *args, &block)
+    else
+      super
+    end
+  end
+  
 end
