7c5338660c38a6141b149ac037ae39eee2d39f18
sathish <>
2018-12-11 09:31:19 +0530

    auth resolver for all queries

---
+++ app/graphql/resolvers/authorize_user.rb
@@ -0,0 +1,15 @@
+module Resolvers
+    class AuthorizeUser
+      def initialize(resolve_func)
+        @resolve_func = resolve_func
+      end
+
+      def call(obj, args, ctx)
+        if ctx[:current_user].blank?
+          GraphQL::ExecutionError.new('User not signed in')
+        else
+          @resolve_func.call(obj, args, ctx)
+        end
+      end
+    end
+end
