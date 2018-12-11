a9a0dd65018720396091e0e67451af706652b0f7
sathish <>
2018-12-11 06:10:37 +0530

    Specs#ExpenseProfile  #1 validations

--- spec/models/expense_profile_spec.rb
+++ spec/models/expense_profile_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe ExpenseProfile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	before(:all) { 
+		@user = build(:user) 
+		@expense_profile = build(:expense_profile,user_id: @user.id)
+		@expense_profile_overflow = build(:expense_profile, :overflowing_starts_at_day, user_id: @user.id)
+		@expense_profile_negative = build(:expense_profile, :negative_funds, user_id: @user.id)
+	} 		
+
+	context "has proper validations" do
+		it { should validate_presence_of(:starts_at_day) }
+		it { should validate_presence_of(:funds) }
+		it { should validate_numericality_of(:starts_at_day) }
+		it { should validate_numericality_of(:funds) }
+	end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 	
 	before(:all) { 
-		@user = create(:user) 
+		@user = build(:user) 
 		@user_with_crpt_email = build(:user,:incorrect_email)
 		@user_with_crpt_uname = build(:user,:incorrect_uname)
 	} 		
@@ -19,16 +19,16 @@ RSpec.describe User, type: :model do
 		it { should have_one(:expense_profile) }
 		it { should have_many(:my_expense_types) }
 		it { should have_many(:monthly_expense_cycles) }
-		it { should have_and_belong_to_many(:shared_expense_cycles) }
+		it { should have_many(:shared_expense_cycles).through(:participants) }
 	end
 	
-	context "has proper values" do		
+	context "has proper values, when created with proper values," do		
 		it { expect(@user.uname.match /^[\w\s-]*$/).to_not be(nil) }
 		it { expect(@user.email.match /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/).to_not be(nil) }
 		it { expect(@user).to be_valid }
 	end
 
-	context "has no corrupted data" do
+	context "has no corrupted data, when created with corrupted values," do
 		it { expect(@user_with_crpt_email.email.match /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/).to be(nil) }
 		it { expect(@user_with_crpt_email).to be_invalid }
 		it { expect(@user_with_crpt_uname).to be_invalid }
